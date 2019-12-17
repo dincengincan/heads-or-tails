b6024c39e9c033ce68fa3ff96809ecdad4ac0a45
dincengincan <engincandinc@gmail.com>
2019-12-17 22:46:39 +0300

    Updates

---
+++ precache-manifest.7c32f07d8f789226d6f78425ac9ce599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a649748020e8f313c17ce106d4e2606",
+    "url": "/heads-or-tails-app/index.html"
+  },
+  {
+    "revision": "0466a2b4dee47e5ae645",
+    "url": "/heads-or-tails-app/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "571605b72f5c2947f2bb",
+    "url": "/heads-or-tails-app/static/js/2.95c61bb0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/heads-or-tails-app/static/js/2.95c61bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0466a2b4dee47e5ae645",
+    "url": "/heads-or-tails-app/static/js/main.1ecde27b.chunk.js"
+  },
+  {
+    "revision": "69227d7e77c08991025d",
+    "url": "/heads-or-tails-app/static/js/runtime-main.1516c764.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/heads-or-tails-app/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/heads-or-tails-app/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
